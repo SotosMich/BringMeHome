510e62685e2d9fff70b3323bed8f91ca38c7034f
SotosMich <sotos_m@hotmail.com>
2019-02-16 12:37:32 +0000

    fixed static files

--- BringMeHome/urls.py
+++ BringMeHome/urls.py
@@ -17,12 +17,14 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from web_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
             url(r'^$', views.index, name='index'),
-            url(r'^rango/', include('rango.urls')),
+            url(r'^web_app/', include('web_app.urls')),
             # above maps any URLs starting
-            # with rango/ to be handled by
-            # the rango application
+            # with web_app/ to be handled by
+            # the web_app application
             url(r'^admin/', admin.site.urls),
         ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
