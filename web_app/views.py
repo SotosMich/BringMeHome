4c61b33925a1f180dd72ff0a8b7cdd4ce544068d
SotosMich <sotos_m@hotmail.com>
2019-02-16 14:14:09 +0000

    fixed template directory

--- web_app/views.py
+++ web_app/views.py
@@ -6,13 +6,11 @@ from django.http import HttpResponse
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'web_app/index.html')
 
-
-    return HttpResponse("Rango says hey there partner!")
 def about(request):
-    return render(request, 'rango/about.html')
+    return render(request, 'web_app/about.html')
