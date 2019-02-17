7d74e845c44a8488ad3bac27b8f809690851ac1c
SotosMich <sotos_m@hotmail.com>
2019-02-17 16:08:30 +0000

    minor changes

---
+++ BringMeHome/populate.py
@@ -0,0 +1,62 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BringMeHome.settings')
+import django
+
+django.setup()
+from web_app.models import Post, Comment
+
+
+def populate():
+    # First, we will create lists of dictionaries containing the pages
+    # we want to add into each category.
+    # Then we will create a dictionary of dictionaries for our categories.
+    # This might seem a little bit confusing, but it allows us to iterate
+    # through each data structure, and add the data to our models.
+
+    comments = [
+        {"commentid": 1, "text": "kjasakjs", "date": "2014-12-12 13:30"},
+        {"commentid": 2, "text": "dfgd", "date": "2015-12-12 13:30"}]
+
+    posts = {1: {"comments": comments, "date": "2016-12-12 13:30", "text": "dsadada", "status": False}}
+
+    # If you want to add more catergories or pages,
+    # add them to the dictionaries above.
+    # The code below goes through the cats dictionary, then adds each category,
+    # and then adds all the associated pages for that category.
+    # if you are using Python 2.x then use cats.iteritems() see
+    # http://docs.quantifiedcode.com/python-anti-patterns/readability/
+    # for more information about how to iterate over a dictionary properly.
+
+    for post, post_data in posts.items():
+        p = add_post(post, post_data["date"], post_data["text"], post_data["status"])
+        for c in post_data["comments"]:
+            add_comment(p, c["commentid"], c["text"], c["date"])
+
+    # Print out the categories we have added.
+    for p in Post.objects.all():
+        for c in Comment.objects.filter(postId=p):
+            print("- {0} - {1}".format(str(p), str(c)))
+
+
+def add_comment(postid, commentid, text, date):
+    c = Comment.objects.get_or_create(postId=postid, commentId=commentid)[0]
+    c.text = text
+    c.date = date
+    c.save()
+    return c
+
+
+def add_post(postid, date, text, status):
+    p = Post.objects.get_or_create(postId=postid)[0]
+    p.date = date
+    p.text = text
+    p.status = status
+    p.save()
+    return p
+
+
+# Start execution here!
+if __name__ == '__main__':
+    print("Starting BringMeHome population script...")
+    populate()
--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,40 @@
+# Create your models here.
 from django.db import models
+# from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    # postId = models.ForeignKey(Category, on_delete=models.PROTECT)
+    postId = models.AutoField(primary_key=True)
+    date = models.DateTimeField(max_length=50, null=True)
+    text = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='post_images', blank=True)
+    status = models.BooleanField(default=False)
+    userId = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):  # For Python 2, use __unicode__ too
+        return self.postId
+
+
+class Comment (models.Model):
+    commentId = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=256)
+    date = models.DateTimeField(max_length=50, null=True)
+    userId = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    postId = models.ForeignKey(Post, on_delete=models.PROTECT, null=True)
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+    # The additional attributes we wish to include.
+    phoneNumber = models.IntegerField()
+    location = models.CharField(max_length=25)
+    photo = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    # Remember if you use Python 2.7.x, define __unicode__ too!
+    def __str__(self):
+        return self.user.username
