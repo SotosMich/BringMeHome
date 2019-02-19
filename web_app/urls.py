3697450c483ea8da8f517a4cd3402c873f4646ca
SotosMich <sotos_m@hotmail.com>
2019-02-19 16:19:21 +0000

    registration login files

--- web_app/urls.py
+++ web_app/urls.py
@@ -3,5 +3,9 @@ from web_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'about/', views.about, name='about')
+    url(r'about/', views.about, name='about'),
+    url(r'^register/$',views.register,name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^logout/$', views.user_logout, name='logout')
 ]
