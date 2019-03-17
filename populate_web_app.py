1b8ac5d8a65f6072226b59b8ffd5a4bdb6e82acd
Petruntan <36708390+Petruntan@users.noreply.github.com>
2019-03-17 19:30:11 +0000

    populate script

--- populate_web_app.py
+++ populate_web_app.py
@@ -4,58 +4,77 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BringMeHome.settings')
 import django
 
 django.setup()
-from web_app.models import Post, Comment
+from web_app.models import Post, Comment ,  UserProfile
+from django.contrib.auth.models import User
+
 import datetime
 from django.utils import timezone
 
 
 def populate():
-    # First, we will create lists of dictionaries containing the pages
-    # we want to add into each category.
-    # Then we will create a dictionary of dictionaries for our categories.
-    # This might seem a little bit confusing, but it allows us to iterate
-    # through each data structure, and add the data to our models.
+    python_posts = [
+        {"Title": "Lost dog Number 1",
+         "Text" : "I found this dog on the street",
+         "Status" : "1",
+         "UserId": "1",
+         "postId":"1"},
+         {"Title": "Lost dog Number 2",
+         "Text" : "I found this dog on the sideway",
+         "Status" : "1",
+         "UserId": "2",
+         "postId":"2"},
+         {"Title": "Lost dog Number 3",
+         "Text" : "Una doga perfecta",
+         "Status" : "1",
+         "UserId": "3",
+         "postId":"3"}]
 
-    comments = [
-        {"commentid": 1, "text": "kjasakjs", "date": "2019-02-05 00:00:00+00:00"},
-        {"commentid": 2, "text": "dfgd", "date": "2019-02-05 00:00:00+00:00"}]
 
-    posts = {1: {"comments": comments, "date": "2019-02-05 00:00:00+00:00", "text": "dsadada", "status": False}}
 
-    # If you want to add more catergories or pages,
-    # add them to the dictionaries above.
-    # The code below goes through the cats dictionary, then adds each category,
-    # and then adds all the associated pages for that category.
-    # if you are using Python 2.x then use cats.iteritems() see
-    # http://docs.quantifiedcode.com/python-anti-patterns/readability/
-    # for more information about how to iterate over a dictionary properly.
+    python_comments = [
+    {"Text" : "nice dog mate",
+     "UserId": "petros",
+     "commentid" : "1",
+     "postId":"1"
+    },
+     {"Text" : "Wow mate",
+     "UserId": "petros",
+     "commentid" : "2",
+     "postId":"2"
+    }]
+    
+    for post in python_posts:
+        add_post(post["Title"],
+                 post["Text"],
+                 post["Status"],
+                 post["UserId"],
+                 post["postId"])
 
-    for post, post_data in posts.items():
-        p = add_post(post, post_data["date"], post_data["text"], post_data["status"])
-        for c in post_data["comments"]:
-            add_comment(p, c["commentid"], c["text"], c["date"])
+    # for comm in python_comments:
+    #     add_comm(comm["Text"],
+    #              comm["UserId"],
+    #              comm["commentid"],
+    #              comm["postId"])
 
     # # Print out the categories we have added.
-    # for p in Post.objects.all():
-    #     for c in Comment.objects.filter(postId=p):
-            # print("- {0} - {1}".format(str(p), str(c)))
-
-
-def add_comment(postid, commentid, text, date):
-    c = Comment.objects.get_or_create(postId=postid, commentId=commentid)[0]
-    c.text = text
-    c.date = date
-    c.save()
-    return c
-
-
-def add_post(postid, date, text, status):
-    p = Post.objects.get_or_create(postId=postid)[0]
-    p.date = date
+def add_post(title, text, status, UserId, postId):
+    p = Post.objects.get_or_create(postId=postId)[0]
+    p.title = title
     p.text = text
     p.status = status
+    p.user = UserId
     p.save()
     return p
+
+# def add_comm(text, UserId, commentid , postId):
+#     c = Comment.objects.get_or_create(commentId = commentid)[0]
+#     c.text = text
+#     user = User.objects.get(id=UserId)
+#     c.userId = userId
+#     c.postId = postId
+#     c.save()
+#     return c
+
 
 
 # Start execution here!
